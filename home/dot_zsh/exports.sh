3265658f2bb6e63e48c718a965f38213d7fe2567
Ruslan Mukhametshin <rusmux73@gmail.com>
2025-04-03 10:28:21 +0300

    Initial commit

---
+++ home/dot_zsh/exports.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Atuin handles history
+export HISTFILE=
+export SAVEHIST=0
+
+export EDITOR="micro"
+export LESSHISTFILE=-
+export LESSOPEN="|$HOMEBREW_PREFIX/bin/lesspipe.sh %s"
+export GIT_LFS_SKIP_SMUDGE=1
+export BUILDX_BAKE_ENTITLEMENTS_FS=0 # disable Docker buildx checks
+export PYTORCH_ENABLE_MPS_FALLBACK=1
+export BAT_THEME="OneHalfDark"
+export HOMEBREW_CASK_OPTS="--appdir=/Applications/My"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+export KICS_QUERIES_PATH="$HOMEBREW_PREFIX/opt/kics/share/kics/assets/queries"
+
+CHZ_DIR=$(dirname "$(chezmoi source-path)")
+export CHZ_DIR
+
+# pnpm
+export PNPM_HOME="$HOME/Library/pnpm"
+case ":$PATH:" in
+*":$PNPM_HOME:"*) ;;
+*) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
